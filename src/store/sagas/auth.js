dd7e8945934f44a423d2936bf987ec04c82e2813
Marcos R F Rosada <mrfreitasrosada@europe.corp.altran.com>
2020-01-27 17:03:03 +0000

    Duck, Saga and Axios setted

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -8,7 +8,7 @@ export function* signIn({ email, password }) {
   try {
     const response = yield call(api.post, "login", { email, password });
 
-    localStorage.setItem('"Ommi:token', response.data);
+    localStorage.setItem("@Ommi:token", response.data);
     yield put(AuthActions.signInSuccess(response.data));
   } catch (error) {
     console.log(error);
