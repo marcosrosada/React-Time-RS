6e59c2035a0ddeb435cf79609b577813a5337a2b
Marcos R F Rosada <mrfreitasrosada@europe.corp.altran.com>
2020-01-27 16:38:40 +0000

    Duck, Saga and Axios setted

--- src/pages/Auth/SignIn/index.js
+++ src/pages/Auth/SignIn/index.js
@@ -1,23 +1,69 @@
-import React from "react";
+import React, { Component } from "react";
+import propTypes from "prop-types";
+
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import AuthActions from "../../../store/ducks/auth";
 
 import Button from "../../../styles/components/Button";
 import { Container, SignForm } from "../styles";
-const SignIn = () => (
-  <Container>
-    <SignForm onSubmit={() => {}}>
-      <h1>Boas Vindas</h1>
 
-      <span>E-Mail</span>
-      <input type="email" name="email" />
+class SignIn extends Component {
+  static propTypes = {
+    signInRequest: propTypes.func.isRequired
+  };
 
-      <span>Senha</span>
-      <input type="password" name="password" />
+  state = {
+    email: "",
+    password: ""
+  };
 
-      <Button size="big" type="submit">
-        Entrar
-      </Button>
-    </SignForm>
-  </Container>
-);
+  handleSubmit = e => {
+    e.preventDefault();
 
-export default SignIn;
+    const { email, password } = this.state;
+    const { signInRequest } = this.props;
+
+    signInRequest(email, password);
+  };
+
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    const { email, password } = this.state;
+    return (
+      <Container>
+        <SignForm onSubmit={this.handleSubmit}>
+          <h1>Boas Vindas</h1>
+
+          <span>E-Mail</span>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={this.handleInputChange}
+          />
+
+          <span>Senha</span>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={this.handleInputChange}
+          />
+
+          <Button size="big" type="submit">
+            Entrar
+          </Button>
+        </SignForm>
+      </Container>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(AuthActions, dispatch);
+
+export default connect(null, mapDispatchToProps)(SignIn);
---
+++ src/store/sagas/auth.js
@@ -0,0 +1,16 @@
+import { call, put } from "redux-saga/effects";
+
+import api from "../../services/api";
+
+import AuthActions from "../ducks/auth";
+
+export function* signIn({ email, password }) {
+  try {
+    const response = yield call(api.post, "login", { email, password });
+
+    localStorage.setItem('"Ommi:token', response.data);
+    yield put(AuthActions.signInSuccess(response.data));
+  } catch (error) {
+    console.log(error);
+  }
+}
