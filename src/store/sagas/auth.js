bf5d3355034c57c97637840c3fdf43cbd1a9f908
Marcos R F Rosada <mrfreitasrosada@europe.corp.altran.com>
2020-01-28 10:05:14 +0000

    Auth

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -1,12 +1,16 @@
-import { call, put } from "redux-saga/effects";
+import { /* call, */ put } from "redux-saga/effects";
 
-import api from "../../services/api";
+// import api from "../../services/api";
 
 import AuthActions from "../ducks/auth";
 
 export function* signIn({ email, password }) {
   try {
-    const response = yield call(api.post, "login", { email, password });
+    // const response = yield call(api.post, "login", { email, password });
+    const response = {
+      data:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1hcmNvc2Zyb3NhZGFAZ21haWwuY29tIiwiaWF0IjoxNTgwMjA1NTcyLCJleHAiOjE1ODAyMDkxNzIsInN1YiI6IjIifQ.PEZo1cd2qkINwuMOMNiyWIBPblQgnJUOj8FV4GVLZ0c"
+    };
 
     localStorage.setItem("@Ommi:token", response.data);
     yield put(AuthActions.signInSuccess(response.data));
