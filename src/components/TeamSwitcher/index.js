65761fedf8766100b4ba5c55f2b29e291f8a40f8
Marcos R F Rosada <mrfreitasrosada@europe.corp.altran.com>
2020-01-30 11:45:09 +0000

    Ducks and Saga Call Teams

--- src/components/TeamSwitcher/index.js
+++ src/components/TeamSwitcher/index.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import { TeamsActions } from "../../store/ducks/teams";
+import TeamsActions from "../../store/ducks/teams";
 
 import { Container, TeamList, Team } from "./styles";
 
--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -4,12 +4,12 @@ import { signIn } from "./auth";
 import { AuthTypes } from "../ducks/auth";
 
 import { getTeams } from "./teams";
-import { TeamTypes } from "../ducks/teams";
+import { TeamsTypes } from "../ducks/teams";
 
 export default function* rootSaga() {
   return yield all([
     takeLatest(AuthTypes.SIGN_IN_REQUEST, signIn),
 
-    takeLatest(TeamTypes.GET_TEAMS_REQUEST, getTeams)
+    takeLatest(TeamsTypes.GET_TEAMS_REQUEST, getTeams)
   ]);
 }
