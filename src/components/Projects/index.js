9f9474021c0431293fa6a561157839a9ec82fa23
Marcos R F Rosada <mrfreitasrosada@europe.corp.altran.com>
2020-02-03 10:55:27 +0000

    Project layout

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import { connect } from "react-redux";
 
@@ -36,6 +37,12 @@ const Projects = ({ activeTeam }) => {
       </Project>
     </Container>
   );
+};
+
+Projects.propTypes = {
+  activeTeam: PropTypes.shape({
+    name: PropTypes.string
+  }).isRequired
 };
 
 const mapStateToProps = state => ({
